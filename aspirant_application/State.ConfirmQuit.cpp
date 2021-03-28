3b36fc2f8e98d9ed9c7f8226e2047fce0089dd4f
ernestpazera <ernestpazera@gmail.com>
2021-03-28 04:28:10 -0500

    confirm quit despqghettified

--- aspirant_application/State.ConfirmQuit.cpp
+++ aspirant_application/State.ConfirmQuit.cpp
@@ -1,14 +1,8 @@
 #include "State.ConfirmQuit.h"
-#include "Application.h"
-#include "UIState.h"
 #include "Graphics.Layouts.h"
-#include <map>
-#include "MenuItem.h"
-#include "Data.Strings.h"
+#include "Application.h"
 namespace state::ConfirmQuit
 {
-	const std::string CONFIRM_ITEM_NO_COLOR_NAME = "ConfirmQuit.Color.No";
-	const std::string CONFIRM_ITEM_YES_COLOR_NAME = "ConfirmQuit.Color.Yes";
 	const std::string LAYOUT_NAME = "State.ConfirmQuit";
 	const std::string MENU_ID = "ConfirmQuit";
 
@@ -18,18 +12,9 @@ namespace state::ConfirmQuit
 		YES
 	};
 
-	static ConfirmQuitItem current = ConfirmQuitItem::NO;
-	static std::map<ConfirmQuitItem, ::MenuItem<ConfirmQuitItem>> items = 
-	{
-		{ConfirmQuitItem::YES,
-			::MenuItem<ConfirmQuitItem>(CONFIRM_ITEM_YES_COLOR_NAME, ConfirmQuitItem::NO, ConfirmQuitItem::NO)},
-		{ConfirmQuitItem::NO,
-			::MenuItem<ConfirmQuitItem>(CONFIRM_ITEM_NO_COLOR_NAME, ConfirmQuitItem::YES, ConfirmQuitItem::YES)}
-	};
-
 	static void ActivateItem()
 	{
-		switch (current)
+		switch ((ConfirmQuitItem)graphics::Layouts::GetMenuValue(LAYOUT_NAME,MENU_ID).value())
 		{
 		case ConfirmQuitItem::NO:
 			::Application::SetUIState(::UIState::MAIN_MENU);
@@ -45,10 +30,10 @@ namespace state::ConfirmQuit
 		switch (command)
 		{
 		case ::Command::UP:
-			MenuItem<ConfirmQuitItem>::Previous(current, items);
+			graphics::Layouts::PreviousMenuIndex(LAYOUT_NAME, MENU_ID);
 			break;
 		case ::Command::DOWN:
-			MenuItem<ConfirmQuitItem>::Next(current, items);
+			graphics::Layouts::NextMenuIndex(LAYOUT_NAME, MENU_ID);
 			break;
 		case ::Command::GREEN:
 			ActivateItem();
@@ -59,15 +44,9 @@ namespace state::ConfirmQuit
 		}
 	}
 
-	static void OnUpdate(const Uint32& ticks)
-	{
-		MenuItem<ConfirmQuitItem>::Update(items, current);
-	}
-
 	void Start()
 	{
 		::Application::SetCommandHandler(::UIState::CONFIRM_QUIT, OnCommand);
 		::Application::SetRenderLayout(::UIState::CONFIRM_QUIT, LAYOUT_NAME);
-		::Application::SetUpdateHandler(::UIState::CONFIRM_QUIT, OnUpdate);
 	}
-}//before = 73
+}
