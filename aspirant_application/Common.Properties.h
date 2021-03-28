035e98338987815e9f74e9809fd95e7aec5f79d6
ernestpazera <ernestpazera@gmail.com>
2021-03-28 06:44:27 -0500

    alphabetized my string constants

--- aspirant_application/Common.Properties.h
+++ aspirant_application/Common.Properties.h
@@ -2,10 +2,10 @@
 #include <string>
 namespace common::Properties
 {
-	const std::string X = "x";
-	const std::string Y = "y";
-	const std::string WIDTH = "width";
 	const std::string HEIGHT = "height";
 	const std::string NAME = "name";
 	const std::string TYPE = "type";
+	const std::string WIDTH = "width";
+	const std::string X = "x";
+	const std::string Y = "y";
 }
--- aspirant_application/Game.Properties.h
+++ aspirant_application/Game.Properties.h
@@ -2,12 +2,12 @@
 #include <string>
 namespace game::Properties
 {
-	const std::string ROOMS = "rooms";
-	const std::string ROOM = "room";
 	const std::string AVATAR = "avatar";
-	const std::string OBJECTS = "objects";
 	const std::string BRIEF = "brief";
 	const std::string FILE_NAME = "fileName";
 	const std::string ID = "id";
+	const std::string OBJECTS = "objects";
+	const std::string ROOM = "room";
+	const std::string ROOMS = "rooms";
 
 }
